ff0d74da996da6556d00e8c784ac0492d5fe3d0b
SoraTrails <me876657621@126.com>
2019-05-01 20:52:06 +0800

    add origin model

--- src/cnn.h
+++ src/cnn.h
@@ -94,7 +94,7 @@ public:
 	CNN();
 	~CNN();
 
-	void init();
+	void init(char* model=NULL);
 	bool train();
 	int  predict(const unsigned char *data, int width, int height);
 	bool readModelFile(const char *name);
